9148b3b16a8c5b545f12f82de22297f0f6bd3e5a
Kalon <kaloncpu57@gmail.com>
2015-11-15 01:13:37 -0700

    Update options.js

--- options.js
+++ options.js
@@ -29,61 +29,6 @@ optionButton.addEventListener("click", toggleOptions);
 menu.insertBefore(document.createElement("br"), optionList); 
 menu.insertBefore(optionButton, optionList);
 
-/*
-var popalert = document.querySelector("input[name=popalert]"),
-    pmalert = document.querySelector("input[name=pmalert]");
-if (getCookie("popint") == "true") {
-  popalert.checked = true;
-}
-if (getCookie("pmint") == "true") {
-  pmalert.checked = true;
-}
-popalert.addEventListener("change", function () {
-  if (this.checked) {
-    setCookie("popint", "true", 1000);
-    startPopInt();
-    if (Notification.permission == "default" || Notification.permission == "denied") {
-      Notification.requestPermission(function () {
-        popNotify("You will now receive alerts about popups automatically, without having to refresh the page.", {close: 3000});
-      });
-    } else {
-      popNotify("You will now receive alerts about popups automatically, without having to refresh the page.", {close: 3000});
-    }
-  } else {
-    delCookie("popint");
-    popNotify("You will no longer receive auto updates for popups.", {close: 3000});    
-  }
-});
-pmalert.addEventListener("change", function () {
-  if (this.checked) {
-    setCookie("pmint", "true", 1000);
-    startPopInt();
-    if (Notification.permission == "default" || Notification.permission == "denied") {
-      Notification.requestPermission(function () {
-        popNotify("You will now receive alerts about PMs automatically, without having to refresh the page.", {close: 3000});
-      });
-    } else {
-      popNotify("You will now receive alerts about PMs automatically, without having to refresh the page.", {close: 3000});
-    }
-  } else {
-    delCookie("pmint");
-    popNotify("You will no longer receive auto updates for PMs.", {close: 3000});
-  }
-});
-var nosnow = optionList.querySelector("input[name=nosnow]");
-if (getCookie("nosnow") == "true") {
-  nosnow.checked = true;
-}
-nosnow.addEventListener("change", function () {
-  if (this.checked) {
-    snowStorm.stop();
-    setCookie("nosnow", "true", 365);
-  } else {
-    snowStorm.init();
-    setCookie("nosnow", "false", 365);
-  }
-});
-*/
 function addOption(option) {
   var label = document.createElement("label");
   var span = document.createElement("span");
