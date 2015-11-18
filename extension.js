d97a6ab9a8c8bcadc91df3821271f24b527c026e
Kalon <kaloncpu57@gmail.com>
2015-11-18 01:23:43 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -10,8 +10,22 @@
 // ==/UserScript==
 
 function anchorJax() {
-
+  window.addEventListener("popstate", fucntion () {
+    $('body').load(this.location.href, null, anchorJax);
+  });
+  var anchors = document.querySelectorAll("a");
+  for (var i = 0; i < anchors.length; i++) {
+    if (anchors[i].href.substring(0, 11) !== "javascript:") {
+      anchors[i].addEventListener("click", function (e) {
+        e.preventDefault();
+        window.history.pushState(null, null, this.href);
+        $('body').load(this.href, null, anchorJax);
+      });
+    }
+  }
 }
+
+anchorJax();
 
 var shoutHeaders = document.querySelectorAll("h2.A");
 for (var i = 0; i < shoutHeaders.length; i++) {
