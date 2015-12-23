685c69d6ca28e12f47b0733c1def65b290e65497
Kalon <kaloncpu57@gmail.com>
2015-12-22 17:37:35 -0700

    Create icon-upload-extension.js

---
+++ icon-upload-extension.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Wapka Icon Uploader Extension
+// @namespace    http://github.com/kaloncpu57
+// @version      0.1
+// @description  Makes adding icons easier
+// @author       kaloncp57
+// @match        http://*.wapka.mobi/edit_*.xhtml?*action=edit_new4_upl*
+// @grant        none
+// ==/UserScript==
+
+var form = document.querySelector("form");
