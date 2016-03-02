21e76c2dcfeaeb2138cbb6a7adec542a2a6efcba
Kalon <kaloncpu57@gmail.com>
2016-03-02 14:52:15 -0700

    Update snow.js

--- snow.js
+++ snow.js
@@ -17,7 +17,7 @@ var snowStorm = (function (window, document) {
 
     // --- common properties ---
 
-    this.autoStart = true; // Whether the snow should start automatically or not.
+    this.autoStart = false; // Whether the snow should start automatically or not.
     this.excludeMobile = false; // Snow is likely to be bad news for mobile phones' CPUs (and batteries.) Enable at your own risk.
     this.flakesMax = 64; // Limit total amount of snow made (falling + sticking)
     this.flakesMaxActive = 32; // Limit amount of snow falling at once (less = lower CPU use)
